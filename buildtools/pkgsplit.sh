8053eed9f45f842a50cae81f82dc7fbdb83f1211
lstux <el@ruedesrancy.org>
2019-11-08 15:19:37 +0100

    feat stripbins/pkgsplit.sh : choose stripping level and enhanced output

--- buildtools/pkgsplit.sh
+++ buildtools/pkgsplit.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 VERBOSE=${VERBOSE:-0}
 
 DEVPKG=false
@@ -10,6 +9,7 @@ DOCPKG=false
 DOCPATTERNS="*/man/* */info/* README"
 
 BINSTRIP=false
+STRIPMODE="unneeded"
 DOCCOMPRESS=false
 COMPRESSOR="xz"
 COMPRESSION_LEVEL="9"
@@ -26,7 +26,9 @@ usage() {
   printf "  -dev     : split into dev package (${DEVPATTERNS})\n"
   printf "  -lib     : split into lib package (${LIBPATTERNS})\n"
   printf "  -doc     : split into doc package (${DOCPATTERNS})\n"
-  printf "  -s       : strip binaries\n"
+  printf "  -s       : strip unneeded symbols from binaries\n"
+  printf "  -S       : strip all symbols from binaries\n"
+  printf "  -D       : strip debug symbols only from binaries"
   printf "  -c       : compress docs\n"
   printf "  -C tool  : use tool to compress docs {gzip|bzip2|[xz]}\n"
   printf "  -0 .. -9 : specify compression level (default is ${COMPRESSION_LEVEL})\n"
@@ -60,7 +62,7 @@ filemove() {
 }
 
 
-while getopts d:l:scC0123456789vh opt do; case "${opt}" in
+while getopts d:l:sSDcC0123456789vh opt do; case "${opt}" in
   d) case "${OPTARG}" in
        "ev") DEVPKG=true;;
        "oc") DOCPKG=true;;
@@ -70,7 +72,9 @@ while getopts d:l:scC0123456789vh opt do; case "${opt}" in
        "ib") LIBPKG=true;;
        *)    usage;;
      esac;;
-  s) BINSTRIP=true;;
+  s) BINSTRIP=true; STRIPMODE="unneeded";;
+  S) BINSTRIP=true; STRIPMODE="all";;
+  D) BINSTRIP=true; STRIPMODE="debug";;
   c) DOCCOMPRESS=true;;
   C) case "${OPTARG}" in
        gzip|bzip2|xz) COMPRESSOR="${OPTARG}";;
@@ -88,16 +92,20 @@ INSTALLDIR="${1}"
 INSTALLDIR="$(echo "${INSTALLDIR}" | sed 's/\/$//')"
 
 ${BINSTRIP} || ${DOCCOMPRESS} || ${DEVPKG} || ${LIBPKG} || ${DOCPKG} || usage "at least one of -dev, -lib, -doc, -s or -c option is required"
+TOTALSIZE="$(du -s "${INSTALLDIR}" | awk '{print $1}')"
 
 if ${BINSTRIP}; then
-  msg "Stripping binaires"
+  msg "Stripping binaries"
   find "${INSTALLDIR}" -type f -executable | while read f; do
     case "$(file "${f}")" in
-      *"not stripped") nfo "  stripping ${f}"; strip --strip-unneeded "${f}";;
+      *"not stripped") nfo "  stripping ${f}"; strip --strip-${STRIPMODE} "${f}";;
       *" stripped")    dbg "  already stripped ${f}";;
-      *)               dbg "  executable unstrippable ${f}";;
+      *)               dbg "  unstrippable executable ${f}";;
     esac
   done
+  BINSIZE="$(du -s "${INSTALLDIR}" | awk '{print $1}')"
+  msg "  gained $(expr ${TOTALSIZE} - ${BINSIZE})Kbytes"
+  TOTALSIZE="${BINSIZE}"
 fi
 
 if ${DOCCOMPRESS}; then
@@ -108,22 +116,28 @@ if ${DOCCOMPRESS}; then
       *)                   nfo "  compressing ${f}"; ${COMPRESSOR} -${COMPRESSION_LEVEL} "${f}";;
     esac
   done
+  BINSIZE="$(du -s "${INSTALLDIR}" | awk '{print $1}')"
+  msg "  gained $(expr ${TOTALSIZE} - ${BINSIZE})Kbytes"
+  TOTALSIZE="${BINSIZE}"
 fi
 
 if ${DEVPKG}; then
   msg "Splitting developpement files to '${INSTALLDIR}-dev'"
   eval find \"${INSTALLDIR}\" $(for p in ${DEVPATTERNS}; do printf " -name '${p}' -o"; done | sed 's/ -o$//') | \
     sed "s@${INSTALLDIR}/@@" | while read f; do filemove "${f}" "dev"; done | nfo_stream
+  msg "  size : $(du -s "${INSTALLDIR}-dev" | awk '{print $1}')/${BINSIZE}Kbytes"
 fi
 
 if ${LIBPKG}; then
   msg "Splitting libraries files to '${INSTALLDIR}-lib'"
   eval find \"${INSTALLDIR}\" $(for p in ${LIBPATTERNS}; do printf " -name '${p}' -o"; done | sed 's/ -o$//') | \
     sed "s@${INSTALLDIR}/@@" | while read f; do filemove "${f}" "lib"; done | nfo_stream
+  msg "  size : $(du -s "${INSTALLDIR}-lib" | awk '{print $1}')/${BINSIZE}Kbytes"
 fi
 
 if ${DOCPKG}; then
   msg "Splitting documentation files to '${INSTALLDIR}-doc'"
   eval find \"${INSTALLDIR}\" $(for p in ${DOCPATTERNS}; do printf " -name '${p}' -o"; done | sed 's/ -o$//') | \
     sed "s@${INSTALLDIR}/@@" | while read f; do filemove "${f}" "doc"; done | nfo_stream
+  msg "  size : $(du -s "${INSTALLDIR}-doc" | awk '{print $1}')/${BINSIZE}Kbytes"
 fi
--- buildtools/stripbins.sh
+++ buildtools/stripbins.sh
@@ -1,18 +1,40 @@
 #!/bin/sh
-BINDIR="${1}"
 VERBOSE="${VERBOSE:-0}"
+STRIP_MODE="unneeded"
 
-if ! [ -d "${BINDIR}" ]; then
+usage() {
   exec >&2
-  printf "Usage : $(basename "${0}") bindir\n"
+  printf "Usage : $(basename "${0}") [options] bindir\n"
   printf "  Find executable files in bindir, and strip them if not already\n"
+  printf "  It will strip all unneeded symbols unless -d or -a option is given\n"
+  printf "options :\n"
+  printf "  -a : strip all symbols\n"
+  printf "  -d : strip debug symbols only\n"
+  printf "  -v : increase verbosity level\n"
+  printf "  -h : display this help message\n"
   exit 1
-fi
+}
 
+while getopts advh opt; do case "${opt}" in
+  a) STRIP_MODE="all";;
+  d) STRIP_MODE="debug";;
+  v) VERBOSE="$(expr ${VERBOSE} + 1)";;
+  *) usage;;
+esac; done
+shift $(expr ${OPTIND} - 1)
+BINDIR="${1}"
+[ -d "${BINDIR}" ] || usage
+
+[ ${VERBOSE} -ge 1 ] && BINSIZE="$(du -s "${BINDIR}" | awk '{print $1}')"
 find "${BINDIR}" -type f -executable | while read f; do
   case "$(file "${f}")" in
-    *"not stripped"*) printf "Stripping ${f}\n"; strip --strip-unneeded "${f}";;
+    *"not stripped"*) printf "Stripping ${f}\n"; strip --strip-${STRIP_MODE} "${f}";;
     *"stripped"*)     [ ${VERBOSE} -ge 1 ] && printf "Already stripped ${f}\n";;
     *)                [ ${VERBOSE} -ge 2 ] && printf "Not strippable ${f}\n";;
   esac
 done
+if [ ${VERBOSE} -ge 1 ]; then
+  FINALBINSIZE="$(du -s "${BINDIR}" | awk '{print $1}')"
+  GAINEDSIZE="$(expr ${BINSIZE} - ${FINALBINSIZE})"
+  printf "Gained ${GAINEDSIZE}Kbytes with stripping\n"
+fi
