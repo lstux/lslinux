44b1463d7581b59dc34257e860c05029682996ae
lstux <el@ruedesrancy.org>
2019-11-19 16:55:25 +0100

    feat lsbuild.sh : typo fix and going further...

--- buildtools/lsbuild.sh
+++ buildtools/lsbuild.sh
@@ -31,6 +31,9 @@ VERBOSE=${VERBOSE:-0}
 DEBUG=${DEBUG:-false}
 #check/install build dependencies
 NODEPS=false
+#Step to resume
+RESUME=0
+
 
 #If binaries should be stripped and how (none|debug|unneeded|all)
 STRIPMODE=unneeded
@@ -57,6 +60,8 @@ usage() {
   printf "  Build LSL package from sources\n"
   printf "options :\n"
   printf "  -nodeps : don't check/install build dependencies\n"
+  printf "  -r num  : resume build at specified step number\n"
+  printf "  -s mode : strip binaries with --strip-mode (none|debug|[unneeded]|all)\n"
   printf "  -d      : enable debug mode (verbose++ and confirm at each step)\n"
   printf "  -v      : increase verbosity level\n"
   printf "  -h      : display this help message\n"
@@ -108,30 +113,71 @@ conf_check() {
   fi
 }
 
-bdeps_check() {
-  ${NODEPS} && return 0
-}
-
 parse_opts() {
   OPTIND=0
-  while getopts n:dvh opt; do case "${opt}" in
+  while getopts n:r:s:dvh opt; do case "${opt}" in
     n) case "${OPTARG}" in
          odeps) NODEPS=true;;
          *)     usage;;
        esac;;
+    r) [ ${OPTARG} -gt 0 ] 2>/dev/null || usage "bad resume number, should be integer >= 0"; RESUME="${OPTARG}";;
+    s) case "${OPTARG}" in
+         none|debug|unneeded|all) STRIPMODE="${OPTARG}";;
+         *)                       usage "bad strip mode '${OPTARG}', should be one of none, debug, unneeded or all";;
+       esac
     d) DEBUG=true; VERBOSE=2;;
     v) VERBOSE="$(expr ${VERBOSE} + 1)";;
     *) usage;;
   esac; done
 }
 
+STEPCOUNT=0
 step() {
   local steplabel="${1}"; shift
-  yesno "${steplabel}" y
-  case "$?" in
-    2) printf "${red}Aborting${nrm}" >&2; exit 255;;
-    1) return 1;;
-  esac
+  STEPCOUNT=$((STEPCOUNT + 1))
+  if ${DEBUG} && [ ${STEPCOUNT} -ge ${RESUME} ]; then
+    yesno "${STEPCOUNT} ${steplabel}" y
+    case "$?" in
+      2) printf "${red}Aborting${nrm}" >&2; exit 255;;
+      1) return 1;;
+    esac
+  else
+    printf "${grn}>${nrm} ${STEPCOUNT} ${steplabel}\n"
+    [ ${RESUME} -ne 0 -a ${STEPCOUNT} -ge ${RESUME} ] && return 1
+  fi
+  return 0
+}
+
+bdeps_check() {
+  step "checking build dependencies" || return 1
+  ${NODEPS} && return 0
+}
+
+sources_download() {
+  local link="${1}" dest="${2}"
+  step "downloading sources to '${dest}'" || return 1
+  if ! [ -e "${dest}" ]; then
+    curl -o "${dest}" "${link}" || error "failed to download sources archive from '${link}'" 2
+  else
+    debug "Sources archive already available : ${dest}"
+  fi
+}
+
+sources_extract() {
+  local archive="${1}" srcdir="${2}" extractdir
+  step "extracting sources to '${srcdir}'" || return 1
+  if ! [ -d "${srcdir}" ]; then
+    extractdir="$(dirname "${srcdir}")"
+    case "${archive}" in
+      *.tar.gz|*.tgz)   tar xzf "${archive}" -C "${extractdir}";;
+      *.tar.bz2|*.tbz2) tar xjf "${archive}" -C "${extractdir}";;
+      *.tar.xz|*.txz)   tar xJf "${archive}" -C "${extractdir}";;
+      *.zip|*.ZIP)      unzip "${archive}" -d "${extractdir}";;
+      *)                error "unhandled archive format '$(basename "${archive}")'" 3
+    esac
+  else
+    debug "Sources already available in '${srcdir}', remove folder to extract again"
+  fi
 }
 
 dokconf_set() {
@@ -167,6 +213,7 @@ esac; done
 }
 
 doconf() {
+  step "configuring sources" || return 1
   case "${1}" in
     "") cd "${LSL_BUILDDIR}/${SRCDIRNAME}" && ./configure ${CONFIGOPTS};;
     -*) cd "${LSL_BUILDDIR}/${SRCDIRNAME}" && ./configure "$@";;
@@ -176,12 +223,68 @@ doconf() {
 }
 
 dobuild() {
+  step "building sources" || return 1
   make -C "${LSL_BUILDDIR}/${SRCDIRNAME}" ${MAKEOPTS} || error "build failed"
 }
 
 doinstall() {
+  step "installing" || return 1
   make -C "${LSL_BUILDDIR}/${SRCDIRNAME}" DESTDIR="${LSL_DESTDIR}/${PKG_NAME}-${PKG_VERSION}" install || error "installation failed"
 }
+
+groupcheck() {
+  local groupinfos="${1}"
+  [ -n "${groupinfos}" ] || return 0
+  local gname="${groupinfos%%:*}" gid="${groupinfos##*:}"
+  [ -n "${gname}" -a -n "${gid}" ] || error "bad ADDGROUP format, should be ADDGROUP=\"groupname:gid\"" 7
+  step "adding group '${gname}' with gid ${gid}" || return 1
+  if egrep -q "^${gname}:" /etc/group; then
+    local lgid="$(awk -F":" "/^${gname}:/{print \$3}" /etc/group)"
+    if [ "${lgid}" != "${gid}" ]; then warning "group '${gname}' already present but with gid ${lgid} instead of ${gid}"
+    else info "group '${gname}' already present"; fi
+    return 0
+  fi
+  local lgname="$(awk -F":" "/:x:${gid}:/{print \$1}" /etc/group)"
+  [ -n "${lgname}" ] && error "GID ${gid} is already used by group '${lgname}'" 7
+  groupadd -g${gid} ${gname} || error "failed to add group '${gname}' (gid=${gid})" 7
+}
+
+usercheck() {
+  local userinfos="${1}" uname uid ugroup uhome ushell
+  [ -n "${userinfos}" ] || return 0
+  uname="$(echo "${userinfos}" | awk -F":" '{print $1}')"
+  uid="$(echo "${userinfos}" | awk -F":" '{print $2}')"
+  ugroup="$(echo "${userinfos}" | awk -F":" '{print $3}')"
+  [ -n "${uname}" -a -n "${uid}" -a -n "${ugroup}" ] || error "badd ADDUSER format, should be ADDUSER=\"username:uid:primarygroup[:homedir=/home/username[:shell=/sbin/nologin]]\"" 8
+  uhome="$(echo "${userinfos}" | awk -F":" '{print $4}')"; [ -n "${uhome}" ] || uhome="/home/${uname}"
+  ushell="$(echo "${userinfos}" | awk -F":" '{print $5}')"; [ -n "${ushell}"] || ushell="/sbin/nologin"
+  step "adding user '${uname}' with uid ${uid} (group ${ugroup}, homedir ${uhome}, login shell ${lshell}" || return 1
+  if egrep -q "^${uname}:" /etc/passwd; then
+    local luid lgroup lhome lshell
+    luid="$(awk -F":" "/^${uname}:/{print \$3}" /etc/passwd)"
+    lgroup="$(awk -F":" "/^${uname}:/{print \$4}" /etc/passwd)"; lgroup="$(awk -F":" "/:x:${lgroup}:/{print \$1}" /etc/group)"
+    lhome="$(awk -F":" "/^${uname}:/{print \$6}" /etc/passwd)"
+    lshell="$(awk -F":" "/^${uname}:/{print \$7}" /etc/passwd)"
+    if [ "${luid}" = "${uid}" -a "${lgroup}" = "${ugroup}" -a "${uhome}" = "${lhome}" -a "${lshell}" = "${ushell}" ]; then
+      info "user '${uname}' already present"
+    else
+      local warn_details="";
+      [ "${luid}" != "${uid}" ] && warn_details="${warn_details}uid=${luid} instead of ${uid}, "
+      [ "${lgroup}" != "${ugroup}" ] && warn_details="${warn_details}group=${lgroup} instead of ${ugroup}, "
+      [ "${lhome}" != "${uhome}" ] && warn_details="${warn_details}home=${lhome} instead of ${uhome}, "
+      [ "${lshell}" != "${ushell}" ] && warn_details="${warn_details}shell=${lshell} instead of ${ushell}, "
+      warn_details="$(echo "${warn_details}" | sed 's/, $//')"
+      warning "user '${uname}' already present but with ${warn_details}"
+    fi
+    return 0
+  fi
+  local luname="$(awk -F":" "\$3==${uid} {print \$1}" /etc/passwd)"
+  [ -n "${luname}" ] && error "UID ${uid} is already used by user '${luname}'" 8
+  useradd -c "added by $(basename "${0}")" -r -d "${uhome}" -g "${ugroup}" -s "${ushell}" -u "${uid}" "${uname}" || error "failed to add user '${uname}' (uid=${uid})" 8
+}
+
+
+
 
 parse_opts "$@"; shift $(expr ${OPTIND} - 1)
 BUILDSCRIPT="$(realpath "${1}" 2>/dev/null)"
@@ -193,9 +296,6 @@ parse_opts "$@"; shift $(expr ${OPTIND} - 1)
 conf_check
 
 BSCRIPT="$(basename "${BUILDSCRIPT}" | sed 's/\.\(sh\|lsl\|lsb\)$//')"
-#PKG_NAME="$(echo "${BSCRIPT}" | awk -F"_" '{print $1}')"
-#PKG_VERSION="$(echo "${BSCRIPT}" | awk -F"_" '{print $2}')"
-#PKG_REVISION="$(echo "${BSCRIPT}" | awk -F"_" '{print $3}')"
 PKG_NAME="$(echo "${BSCRIPT}" | sed 's/^\(.\+\)_.\+_[0-9]\+$/\1/')"
 PKG_VERSION="$(echo "${BSCRIPT}" | sed 's/^.\+_\(.\+\)_[0-9]\+$/\1/')"
 PKG_REVISION="$(echo "${BSCRIPT}" | sed 's/^.\+_.\+_\([0-9]\+\)$/\1/')"
@@ -205,34 +305,24 @@ PKG_SRCLINK="$(echo "${PKG_SRCLINK}" | sed -e "s/{{pkgname}}/${PKG_NAME}/g" -e "
 
 ### Download source package to LSL_SRCDIR
 PKG_LOCALARCH="${LSL_SRCDIR}/$(basename "${PKG_SRCLINK}")"
-if ! [ -e "${PKG_LOCALARCH}" ]; then
-  curl -o "${PKG_LOCALARCH}" "${PKG_SRCLINK}" || error "failed to download sources archive from '${PKG_SRCLINK}'" 2
-else
-  debug "Sources archive already available : ${PKG_LOCALARCH}"
-fi
+sources_download "${PKG_SRCLINK}" "${PKG_LOCALARCH}"
 
 ### Extract source package to LSL_BUILDDIR
 SRCDIRNAME="${SRCDIRNAME:-${PKG_NAME}-${PKG_VERSION}}"
-if ! [ -d "${LSL_BUILDDIR}/${SRCDIRNAME}" ]; then
-  case "${PKG_LOCALARCH}" in
-    *.tar.gz|*.tgz)   tar xzf "${PKG_LOCALARCH}" -C "${LSL_BUILDDIR}";;
-    *.tar.bz2|*.tbz2) tar xjf "${PKG_LOCALARCH}" -C "${LSL_BUILDDIR}";;
-    *.tar.xz|*.txz)   tar xJf "${PKG_LOCALARCH}" -C "${LSL_BUILDDIR}";;
-    *.zip|*.ZIP)      unzip "${PKG_LOCALARCH}" -d "${LSL_BUILDDIR}";;
-    *)                error "unhandled archive format '$(basename "${PKG_LOCALARCH}")'" 3
-  esac
-else
-  debug "Sources already available in '${LSL_BUILDDIR}/${SRCDIRNAME}', remove folder to extract again"
-fi
+sources_extract "${PKG_LOCALARCH}" "${LSL_BUILDDIR}/${SRCDIRNAME}"
 
 ### Source ${BUILDSCRIPT} which should take care of configuring/building/installing with helper functions
 source "${BUILDSCRIPT}"
+
+### Create user/group if needed
+groupcheck "${ADDGROUP}"
+usercheck "${ADDUSER}"
 
 ### Strip binaries
 if [ "${STRIPMODE}" != "none" ]; then
   case "${STRIPMODE}" in
     debug|unneeded|all) STRIPOPTS="--strip-${STRIPMODE}";;
-    *)                  error "bad STIPMODE '${STRIPMODE}', supported values are none, debug, unneeded or all";;
+    *)                  error "bad STRIPMODE '${STRIPMODE}', supported values are none, debug, unneeded or all";;
   esac
 fi
 
